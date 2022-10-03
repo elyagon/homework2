67e1b4ea983054e9b22902dcead67206a8faf8cb
elyagon <ehvelina-gg@yandex.ru>
2022-10-03 03:21:01 +0300

    Домашнее задание 2. Часть 3

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,58 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        //Задание 1.2
-        System.out.println("Задание 1.2");
-        int age = 13;
-        if (age >= 18) {
-            System.out.println("Поздравляем Вас с совершеннолетием");
+        //Задание 1
+        System.out.println("Задание 1");
+        int age = 6;
+        if (age >= 2 && age <= 6) {
+            System.out.println("Если возраст человека равен " + age + " , то ему нужно ходить в детский сад");
+        } else if (age >= 7 && age <= 18) {
+            System.out.println("Если возраст человека равен " + age + " , то ему нужно ходить в школу");
+        } else if (age >= 18 && age <= 24) {
+            System.out.println("Если возраст человека равен " + age + " , то ему нужно ходить в университет");
+        } else if (age > 24) {
+            System.out.println("Если возраст человека равен " + age + " , то ему нужно ходить на работу");
         }
-        else {
-            System.out.println("Возраст совершеннолетия еще не наступил, Вам нужно подождать");
-        }
+
 //Задание 2
         System.out.println("Задание 2");
-        int yearsOld = 5;
-        if (yearsOld >= 7 && yearsOld < 18) {
-            System.out.println("Ребенок ходит в школу");
-        }
-        else if (yearsOld >= 18 && yearsOld < 24) {
-            System.out.println("Человек ходит в университет");
-        }
-        else if (yearsOld>=24){
-            System.out.println("Человек окончил университет и ему пора искать первую работу");
+
+        if (age < 5) {
+            System.out.println("Ребенлк не млжет кататься на аттракционе.");
+        } else if (age >= 5 && age < 14) {
+            System.out.println("Ребенок может кататься только в сопровождении взрослого. Если взрослого нет, то кататься нельзя.");
+        } else {
+            System.out.println("Ребенок может кататься без сопровождения взрослого.");
         }
         //Задание 3
         System.out.println("Задание 3");
-        int capacity = 102;
-        int seat = 60;
-        int standingPlace = capacity - seat;
-
-        int usedSeats = 60;
-        int otherUsed = 42;
-
-        if (usedSeats < seat) {
-            System.out.println("Есть еще " + (seat - usedSeats) + " сидячих мест");
-        }
-       else {
-            System.out.println("Сидячих мест нет");
-        }
-        if (otherUsed < standingPlace) {
-            System.out.println("Есть еще " + (standingPlace - otherUsed) + " стоячих мест");
-        }
-
-        else{
-            System.out.println("Стоячих мест нет");
-        }
+        int one = 117;
+        int two = 117;
+        int free = 117;
+        if (one > two) {
+            if (one > free) {
+                System.out.println("Максимальное число - " + one);
+            } else if (free > one) {
+                System.out.println("Максимальное число - " + free);
+            } else
+                System.out.println("Числа" + one + " и " + free + "равны");
+        } else if (two > one) {
+            if (two > free) {
+                System.out.println("Максимальное число - " + two);
+            } else if (free > two) {
+                System.out.println("Максимальное число - " + free);
+            } else
+                System.out.println("Числа" + two + " и " + free + "равны");
+            System.out.println("Максимальное число - " + two);
+        } else {
+            if (one>free){
+                System.out.println("Максимальное число - " + one);
+            } else if (free>one){
+                System.out.println("Максимальное число - " + free);
+            } else {
+                System.out.println("Все числа равны");
+            }
+    }
     }
 }
